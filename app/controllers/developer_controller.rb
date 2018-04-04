754fe4becf049019eff7c2debf146b1897cd305c
ThuPV (Thomas) <thupv.personal@gmail.com>
2018-04-04 15:14:15 +0700

    Change permitted_params to list_params

--- app/controllers/developer_controller.rb
+++ app/controllers/developer_controller.rb
@@ -1,10 +1,10 @@
 class DeveloperController < ApplicationController
   def list
-    @form = SearchDeveloperForm.new(permitted_params)
+    @form = SearchDeveloperForm.new(list_params)
     @developers = @form.search
   end
 
-  def permitted_params
+  def list_params
     params.permit(developers_search_form: SearchDeveloperForm::FORM_FIELDS)
   end
 end
